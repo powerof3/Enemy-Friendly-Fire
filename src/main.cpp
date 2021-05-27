b952e00fb101b8b516c2f9e3437c110d62082d80
powerofthree <dinuk2483@ymail.com>
2021-05-27 21:48:32 +0530

    Add project files.

---
+++ src/main.cpp
@@ -0,0 +1,83 @@
+﻿#include "version.h"
+
+
+class EFF
+{
+public:
+	static void Hook()
+	{
+		REL::Relocation<std::uintptr_t> vtbl{ REL::ID(261397) };  //Character vtbl
+		vtbl.write_vfunc(0x0D6, CheckValidTarget);
+	}
+private:
+	static bool CheckValidTarget(RE::Actor* a_this, RE::Actor& a_ref)
+	{
+		return true;
+	}
+};
+
+
+extern "C" DLLEXPORT bool APIENTRY SKSEPlugin_Query(const SKSE::QueryInterface* a_skse, SKSE::PluginInfo* a_info)
+{
+	try {
+		auto path = logger::log_directory().value() / "po3_EnemyFriendlyFire.log";
+		auto log = spdlog::basic_logger_mt("global log", path.string(), true);
+		log->flush_on(spdlog::level::info);
+
+#ifndef NDEBUG
+		log->set_level(spdlog::level::debug);
+		log->sinks().push_back(std::make_shared<spdlog::sinks::msvc_sink_mt>());
+#else
+		log->set_level(spdlog::level::info);
+
+#endif
+		spdlog::set_default_logger(log);
+		spdlog::set_pattern("[%H:%M:%S] [%l] %v");
+
+		logger::info("Enemy Friendly Fire {}", SOS_VERSION_VERSTRING);
+
+		a_info->infoVersion = SKSE::PluginInfo::kVersion;
+		a_info->name = "Enemy Friendly Fire";
+		a_info->version = SOS_VERSION_MAJOR;
+
+		if (a_skse->IsEditor()) {
+			logger::critical("Loaded in editor, marking as incompatible");
+			return false;
+		}
+
+		const auto ver = a_skse->RuntimeVersion();
+		if (ver < SKSE::RUNTIME_1_5_39) {
+			logger::critical("Unsupported runtime version {}", ver.string());
+			return false;
+		}
+	} catch (const std::exception& e) {
+		logger::critical(e.what());
+		return false;
+	} catch (...) {
+		logger::critical("caught unknown exception");
+		return false;
+	}
+
+	return true;
+}
+
+
+extern "C" DLLEXPORT bool APIENTRY SKSEPlugin_Load(const SKSE::LoadInterface* a_skse)
+{
+	try {
+		logger::info("Enemy Friendly Fire loaded");
+
+		SKSE::Init(a_skse);
+
+		EFF::Hook();
+
+	} catch (const std::exception& e) {
+		logger::critical(e.what());
+		return false;
+	} catch (...) {
+		logger::critical("caught unknown exception");
+		return false;
+	}
+
+	return true;
+}
