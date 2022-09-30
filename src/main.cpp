f8c24ca929a45e37387102b7419c785f96df7794
powerof3 <32599957+powerof3@users.noreply.github.com>
2022-09-30 12:20:10 +0530

    merge SE/AE support 1.6.629+

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-namespace EFF
+namespace EnemyFriendlyFire
 {
 	struct CheckValidTarget
 	{
@@ -17,26 +17,28 @@ namespace EFF
 	}
 }
 
+void OnInit(SKSE::MessagingInterface::Message* a_msg)
+{
+	if (a_msg->type == SKSE::MessagingInterface::kPostLoad) {
+		EnemyFriendlyFire::Install();
+	}
+}
+
+#ifdef SKYRIM_AE
+extern "C" DLLEXPORT constinit auto SKSEPlugin_Version = []() {
+	SKSE::PluginVersionData v;
+	v.PluginVersion(Version::MAJOR);
+	v.PluginName("Enemy Friendly Fire");
+	v.AuthorName("powerofthree");
+	v.UsesAddressLibrary();
+	v.UsesNoStructs();
+	v.CompatibleVersions({ SKSE::RUNTIME_LATEST });
+
+	return v;
+}();
+#else
 extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Query(const SKSE::QueryInterface* a_skse, SKSE::PluginInfo* a_info)
 {
-	auto path = logger::log_directory();
-	if (!path) {
-		return false;
-	}
-
-	*path /= fmt::format(FMT_STRING("{}.log"), Version::PROJECT);
-	auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true);
-
-	auto log = std::make_shared<spdlog::logger>("global log"s, std::move(sink));
-
-	log->set_level(spdlog::level::info);
-	log->flush_on(spdlog::level::info);
-
-	spdlog::set_default_logger(std::move(log));
-	spdlog::set_pattern("[%H:%M:%S:%e] %v"s);
-
-	logger::info(FMT_STRING("{} v{}"), Version::PROJECT, Version::NAME);
-
 	a_info->infoVersion = SKSE::PluginInfo::kVersion;
 	a_info->name = "Enemy Friendly Fire";
 	a_info->version = Version::MAJOR;
@@ -54,14 +56,39 @@ extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Query(const SKSE::QueryInterface* a
 
 	return true;
 }
+#endif
+
+void InitializeLog()
+{
+	auto path = logger::log_directory();
+	if (!path) {
+		stl::report_and_fail("Failed to find standard logging directory"sv);
+	}
+
+	*path /= fmt::format(FMT_STRING("{}.log"), Version::PROJECT);
+	auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true);
+
+	auto log = std::make_shared<spdlog::logger>("global log"s, std::move(sink));
+
+	log->set_level(spdlog::level::info);
+	log->flush_on(spdlog::level::info);
+
+	spdlog::set_default_logger(std::move(log));
+	spdlog::set_pattern("[%H:%M:%S] %v"s);
+
+	logger::info(FMT_STRING("{} v{}"), Version::PROJECT, Version::NAME);
+}
 
 extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface* a_skse)
 {
-	logger::info("loaded plugin");
+	InitializeLog();
+
+	logger::info("Game version : {}", a_skse->RuntimeVersion().string());
 
 	SKSE::Init(a_skse);
 
-	EFF::Install();
+	auto messaging = SKSE::GetMessagingInterface();
+	messaging->RegisterListener("SKSE", OnInit);
 
 	return true;
 }
