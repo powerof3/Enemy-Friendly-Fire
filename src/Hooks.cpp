1e70721e314bcda3cbc1a9ee781afd2019f3a8ea
powerof3 <powerof3@users.noreply.github.com>
2026-01-07 00:36:58 +0530

    Fix VR index

--- src/Hooks.cpp
+++ src/Hooks.cpp
@@ -9,7 +9,11 @@ namespace EnemyFriendlyFire
 			return true;
 		}
 		[[maybe_unused]] static inline REL::Relocation<decltype(thunk)> func;
+#ifndef SKYRIMVR
 		static inline constexpr std::size_t size = 0x0D6;
+#else
+		static inline constexpr std::size_t size = 0x0D8
+#endif
 	};
 
 	void Install()
